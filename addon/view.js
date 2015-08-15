55b99ce522b928d254249bda079d5d93e37bd272
Diogo Mafra <eudega@gmail.com>
2015-08-15 19:16:04 -0300

    Defining a css class to the hellgate container.

--- addon/view.js
+++ addon/view.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 import template from "ember-hellgate/templates/view";
 
 export default Ember.View.extend({
+  classNames: ['hellgate-container'],
   template: template
 });
