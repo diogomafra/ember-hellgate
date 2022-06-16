b214acfc9f2a54f5f7728e0a2f0cf8b5cbb15f44
Adam Woźny <wozny1989@gmail.com>
2022-06-17 01:34:16 +0200

    Fix build app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 module.exports = {
-  name: 'ember-hellgate',
+  name: "ember-hellgate",
 
-  included: function(app) {
-    app.import('vendor/ember-hellgate.css');
-  }
+  included(app, parentAddon) {
+    let target = parentAddon || app;
+    target.options = target.options || {};
+    target.options.babel = target.options.babel || { includePolyfill: true };
+    target.import("vendor/ember-hellgate.css");
+    return this._super.included.apply(this, arguments);
+  },
 };
