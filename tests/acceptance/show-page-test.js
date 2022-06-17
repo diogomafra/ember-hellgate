0d678407757aadc07a96c35ca79261efd52d90f7
Adam Woźny <wozny1989@gmail.com>
2022-06-17 03:41:13 +0200

    Chore: refactor tests

--- tests/acceptance/show-page-test.js
+++ tests/acceptance/show-page-test.js
@@ -1,85 +1,65 @@
-import Ember from 'ember';
-import { module, test } from 'qunit';
-import startApp from '../helpers/start-app';
+import { module, test } from "qunit";
+import { setupApplicationTest } from "ember-qunit";
+import { visit, waitFor, waitUntil, find, click } from "@ember/test-helpers";
 
-module('Acceptance: show page', {
-  beforeEach() {
-    this.application = startApp();
-  },
-  afterEach() {
-    Ember.run(this.application, 'destroy');
+module("Acceptance: show page", function (hooks) {
+  setupApplicationTest(hooks);
+
+  test("displays the correct page", async function (assert) {
+    await visit("/test1");
+    await iframeHasUrlWithContent(assert, "/test1.html", "My first test");
+
+    await visit("/test2");
+    await iframeHasUrlWithContent(assert, "/test2.html", "My second test");
+  });
+
+  test("displays hellgates inside nested routes", async function (assert) {
+    await visit("/nested/page");
+    assert.dom(".header-title").hasText("Nested header");
+    await iframeHasUrlWithContent(
+      assert,
+      "/nested-page.html",
+      "My nested page"
+    );
+  });
+
+  test("displays nested ember pages next to a hellgate", async function (assert) {
+    await visit("/nested");
+    assert.dom(".header-title").hasText("Nested header");
+    assert.dom("span").hasText("My nested index page");
+
+    await visit("/nested/regular");
+    assert.dom(".header-title").hasText("Nested header");
+    assert.dom("span").hasText("My regular Ember page");
+  });
+
+  test("navigates through links", async function (assert) {
+    await visit("/");
+    await click("a.link2");
+    await iframeHasUrlWithContent(assert, "/test2.html", "My second test");
+
+    await visit("/");
+    await click("a.link1");
+    await iframeHasUrlWithContent(assert, "/test1.html", "My first test");
+  });
+
+  test("allows to escape hell", async function (assert) {
+    await visit("/test1");
+    assert.dom("#the-message").hasNoText();
+
+    await visit("/escape");
+    await waitUntil(() => find("#the-message").textContent);
+    assert.dom("#the-message").hasText("hello");
+  });
+
+  async function iframeHasUrlWithContent(assert, url, content) {
+    await waitFor("iframe");
+    assert.dom("iframe").hasAttribute("src", url);
+
+    const nestedSpanElement = await waitUntil(function () {
+      return find("iframe").contentDocument.querySelector("span");
+    });
+
+    assert.equal(nestedSpanElement.textContent, content);
   }
 });
-
-test('displays the correct page', function(assert) {
-  visit('/test1');
-  andThen(function() {
-    iframeHasUrlWithContent(assert, '/test1.html', 'My first test');
-  });
-
-  visit('/test2');
-  andThen(function() {
-    iframeHasUrlWithContent(assert, '/test2.html', 'My second test');
-  });
-});
-
-test('displays hellgates inside nested routes', function(assert) {
-  visit('/nested/page');
-  andThen(function() {
-    assert.equal(find('.header-title').text(), 'Nested header');
-    iframeHasUrlWithContent(assert, '/nested-page.html', 'My nested page');
-  });
-});
-
-test('displays nested ember pages next to a hellgate', function(assert) {
-  visit('/nested');
-  andThen(function() {
-    assert.equal(find('.header-title').text(), 'Nested header');
-    assert.equal(find('span').text(), 'My nested index page');
-  });
-
-  visit('/nested/regular');
-  andThen(function() {
-    assert.equal(find('.header-title').text(), 'Nested header');
-    assert.equal(find('span').text(), 'My regular Ember page');
-  });
-});
-
-test('navigates through links', function(assert) {
-  visit('/');
-  click('a.link2');
-  andThen(function() {
-    iframeHasUrlWithContent(assert, '/test2.html', 'My second test');
-  });
-
-  visit('/');
-  click('a.link1');
-  andThen(function() {
-    iframeHasUrlWithContent(assert, '/test1.html', 'My first test');
-  });
-});
-
-
-test('allows to escape hell', function(assert) {
-  visit('/test1');
-  andThen(function() {
-    Ember.run.later(() => {
-      assert.equal(find('#the-message').text(), '');
-    }, 100);
-  });
-
-  visit('/escape');
-  andThen(function() {
-    Ember.run.later(() => {
-      assert.equal(find('#the-message').text(), 'hello');
-    }, 100);
-  });
-});
-
-function iframeHasUrlWithContent(assert, url, content) {
-  // TODO: instead of waiting 200ms, check when the iframe has loaded.
-  Ember.run.later(() => {
-    assert.equal(find('iframe').attr('src'), url);
-    assert.equal(find("iframe").contents().find('span').text(), content);
-  }, 100);
-}
